6190f8a4209db935a12401d8ce92521da9d33606
Boy Maas <boy.maas@gmail.com>
2022-12-10 20:29:09 +0100

    reading event stream from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,61 @@
-use {blasted::Document, std::str::FromStr};
+use {
+  anyhow::Result,
+  blasted::Document,
+  crossterm::{
+    self,
+    event::{
+      DisableMouseCapture,
+      EnableMouseCapture,
+      Event,
+      EventStream,
+      KeyCode,
+      KeyEvent,
+    },
+    execute,
+    terminal::disable_raw_mode,
+  },
+  futures::StreamExt,
+  std::str::FromStr,
+};
 
-fn main() {
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<()> {
   let _core = Document::from_str("Hello world!");
+
+  // use crossterm to set terminal into raw mode
+  // and then restore it when the program exits
+  // while capturing stdin and read on the EventStream
+  // while printing the events to stdout
+
+  crossterm::terminal::enable_raw_mode().unwrap();
+
+  let mut stdout = std::io::stdout();
+  execute!(stdout, EnableMouseCapture)?;
+
+  let mut reader = EventStream::new();
+  loop {
+    match reader.next().await {
+      Some(Ok(event)) => {
+        println!("{:?}", event);
+        if let Event::Key(KeyEvent {
+          code: KeyCode::Char('q'),
+          ..
+        }) = event
+        {
+          break;
+        }
+      }
+      Some(Err(e)) => {
+        println!("Error: {:?}", e);
+        break;
+      }
+      None => break,
+    }
+  }
+
+  execute!(stdout, DisableMouseCapture)?;
+
+  disable_raw_mode()?;
+
+  Ok(())
 }
